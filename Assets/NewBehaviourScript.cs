6e6177e07357af4e38e98c49a9a32856cc415f1d
Guillaume78ory <guillaume78.ory@gmail.com>
2024-01-26 14:13:05 +0200

    m

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LightSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Light light;
+
+    // Use this for initialization
     void Start()
     {
-        
+        light = GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("tab"))
+        {
+            // You can insert a new color here. For example, to switch to red:
+            light.color = Color.red;
+
+            // If you want to cycle through colors, you could define a list of colors
+            // and select the next one each time the Tab key is pressed.
+            // For example, you could use an array of colors and a variable to keep track of the current index.
+     
+        }
     }
 }
