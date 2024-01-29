e0c353d12d158fb158a054669cc6b07d5b11dd87
Guillaume78ory <guillaume78.ory@gmail.com>
2024-01-29 16:53:14 +0200

    big changes

---
+++ Assets/BreakOut.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Breakout : MonoBehaviour
+{
+    Vector3 m_NewPosition;
+    Vector3 Zero_Position;
+    public float m_XPosition;
+    void Start()
+    {
+        m_NewPosition = new Vector3(30.0f, 0.0f, 0.0f);
+        Zero_Position = transform.position;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (transform.position == m_NewPosition)
+            {
+                transform.position = Zero_Position;
+            }
+            else
+                transform.position = m_NewPosition;
+
+        }
+
+    }
+}
